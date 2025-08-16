c64714b3a1a73b93ae25f75411c412b4a192da0b
Kiran Anand <liteninkiran@hotmail.com>
2025-08-16 17:49:49 +0100

    045 - Creating the reservation service

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+const key = 'reservations';
 
 @Injectable({
     providedIn: 'root',
 })
 export class ReservationService {
+    private reservations: Reservation[] = [];
+
     constructor() {}
+
+    public getReservations(): Reservation[] {
+        return this.reservations;
+    }
+
+    public getReservation(id: string): Reservation | undefined {
+        return this.reservations.find((res) => res.id === id);
+    }
+
+    public addReservation(reservation: Reservation): void {
+        reservation.id = Date.now().toString();
+        this.reservations.push(reservation);
+        localStorage.setItem(key, JSON.stringify(this.reservations));
+    }
+
+    public deleteReservation(id: string): void {
+        const index = this.reservations.findIndex((res) => res.id === id);
+        this.reservations.splice(index, 1);
+        localStorage.setItem(key, JSON.stringify(this.reservations));
+    }
+
+    public updateReservation(id: string, reservation: Reservation): void {
+        const index = this.reservations.findIndex((res) => res.id === id);
+        this.reservations[index] = reservation;
+        localStorage.setItem(key, JSON.stringify(this.reservations));
+    }
 }
