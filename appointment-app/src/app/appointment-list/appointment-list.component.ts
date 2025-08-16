4f0927a52f4c8f33d970c78fcc12d2ad223e12d7
Kiran Anand <liteninkiran@hotmail.com>
2025-08-16 11:56:22 +0100

    025 - Using ngOnInit to load appointments from local storage

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
-import { Appointment, DummyAppointment } from '../models/appointment';
+import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+const key = 'appointments';
 
 @Component({
     selector: 'app-appointment-list',
     templateUrl: './appointment-list.component.html',
     styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
     today: string = new Date().toISOString().split('T')[0];
     appointments: Appointment[] = [];
     newAppointmentTitle: string = '';
     newAppointmentDate: Date = new Date();
 
-    public addAppointment() {
+    public ngOnInit(): void {
+        const savedAppointments = localStorage.getItem(key);
+        this.appointments = savedAppointments
+            ? JSON.parse(savedAppointments)
+            : [];
+    }
+
+    public addAppointment(): void {
         if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
             const appt: Appointment = {
                 id: Date.now(),
@@ -27,14 +36,13 @@ export class AppointmentListComponent {
         }
     }
 
-    public deleteAppointment(index: number) {
+    public deleteAppointment(index: number): void {
         this.appointments.splice(index, 1);
         this.storeAppointments();
     }
 
-    private storeAppointments() {
+    private storeAppointments(): void {
         const data = JSON.stringify(this.appointments);
-        const key = 'appointments';
         localStorage.setItem(key, data);
     }
 }
