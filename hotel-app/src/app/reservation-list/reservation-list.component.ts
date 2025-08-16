ca9a920ddbad7982f6e2d2521904d6a339d76e10
Kiran Anand <liteninkiran@hotmail.com>
2025-08-16 18:10:30 +0100

    048 - Showing all reservations in a list

--- hotel-app/src/app/reservation-list/reservation-list.component.ts
+++ hotel-app/src/app/reservation-list/reservation-list.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { ReservationService } from '../reservation/reservation.service';
 
 @Component({
     selector: 'app-reservation-list',
     templateUrl: './reservation-list.component.html',
     styleUrls: ['./reservation-list.component.css'],
 })
-export class ReservationListComponent {}
+export class ReservationListComponent implements OnInit {
+    public reservations: Reservation[] = [];
+
+    constructor(private reservationService: ReservationService) {}
+
+    public ngOnInit(): void {
+        this.reservations = this.reservationService.getReservations();
+    }
+}
