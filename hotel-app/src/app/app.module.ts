ce5eea386a6330b25fc4a671e25eac004298ed8e
Kiran Anand <liteninkiran@hotmail.com>
2025-08-16 16:47:48 +0100

    039 - Building a simple navigation

--- hotel-app/src/app/app.module.ts
+++ hotel-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, HomeModule, ReservationModule],
     providers: [],
     bootstrap: [AppComponent],
 })
