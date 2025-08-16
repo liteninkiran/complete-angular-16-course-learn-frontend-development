80e53024aabe7cda953163a9f2c94496410ea1b8
Kiran Anand <liteninkiran@hotmail.com>
2025-08-16 17:31:33 +0100

    040 - Creating a form group

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-reservation-form',
     templateUrl: './reservation-form.component.html',
     styleUrls: ['./reservation-form.component.css'],
 })
-export class ReservationFormComponent {}
+export class ReservationFormComponent {
+    reservationForm: FormGroup = new FormGroup({});
+
+    constructor(private formBuilder: FormBuilder) {}
+}
