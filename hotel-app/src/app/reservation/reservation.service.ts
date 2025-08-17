0e48ba2a3e6931ad339e77465851366814dbd38d
Kiran Anand <liteninkiran@hotmail.com>
2025-08-17 15:34:18 +0100

    065 - Preparing the project for the mock backend api

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,20 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/reservation';
 
-const key = 'reservations';
-
 @Injectable({
     providedIn: 'root',
 })
 export class ReservationService {
     private reservations: Reservation[] = [];
-
-    constructor() {
-        const savedReservations = localStorage.getItem(key);
-        this.reservations = savedReservations
-            ? JSON.parse(savedReservations)
-            : [];
-    }
 
     public getReservations(): Reservation[] {
         return this.reservations;
@@ -27,18 +18,15 @@ export class ReservationService {
     public addReservation(reservation: Reservation): void {
         reservation.id = Date.now().toString();
         this.reservations.push(reservation);
-        localStorage.setItem(key, JSON.stringify(this.reservations));
     }
 
     public deleteReservation(id: string): void {
         const index = this.reservations.findIndex((res) => res.id === id);
         this.reservations.splice(index, 1);
-        localStorage.setItem(key, JSON.stringify(this.reservations));
     }
 
     public updateReservation(id: string, reservation: Reservation): void {
         const index = this.reservations.findIndex((res) => res.id === id);
         this.reservations[index] = reservation;
-        localStorage.setItem(key, JSON.stringify(this.reservations));
     }
 }
