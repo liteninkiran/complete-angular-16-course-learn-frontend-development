6bf813b0fb935a5b016bb5d484a385157faa8407
Kiran Anand <liteninkiran@hotmail.com>
2025-08-18 14:43:32 +0100

    090 - Adding a navbar

--- amazing-animal-paintings/src/app/app.module.ts
+++ amazing-animal-paintings/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [AppComponent],
@@ -15,6 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         ProductModule,
         HttpClientModule,
         BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
