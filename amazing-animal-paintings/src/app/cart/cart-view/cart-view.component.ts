311774ad15b04cc41ed097789764991de0c1a1dc
Kiran Anand <liteninkiran@hotmail.com>
2025-08-18 15:13:26 +0100

    096 - Calculating the total cart price

--- amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
+++ amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
@@ -10,12 +10,18 @@ import { Product } from 'src/app/models/product';
 export class CartViewComponent implements OnInit {
     public cartItems: Product[] = [];
     public totalPrice: number = 0;
+    public currency: string = 'GBP';
 
     constructor(private cartService: CartService) {}
 
     public ngOnInit(): void {
         this.cartService.getCartItems().subscribe((data) => {
             this.cartItems = data;
+            this.totalPrice = this.getTotalPrice();
         });
+    }
+
+    public getTotalPrice(): number {
+        return this.cartItems.reduce((sum, item) => sum + item.price, 0);
     }
 }
