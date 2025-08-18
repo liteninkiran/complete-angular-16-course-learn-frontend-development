9026bafcab7f6cec3aa92fb336cf6d5545cc1461
Kiran Anand <liteninkiran@hotmail.com>
2025-08-18 13:05:18 +0100

    085 - Creating mat-cards for products

--- amazing-animal-paintings/src/app/product/product.module.ts
+++ amazing-animal-paintings/src/app/product/product.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     declarations: [ProductListComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatSnackBarModule,
+        MatInputModule,
+        MatSelectModule,
+    ],
 })
 export class ProductModule {}
