ac7f19c9683780d9831969caf074872314a7afaf
Kiran Anand <liteninkiran@hotmail.com>
2025-08-18 15:07:48 +0100

    094 - Getting all cart items

--- amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
+++ amazing-animal-paintings/src/app/cart/cart-view/cart-view.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
     selector: 'app-cart-view',
     templateUrl: './cart-view.component.html',
     styleUrls: ['./cart-view.component.css'],
 })
-export class CartViewComponent {}
+export class CartViewComponent implements OnInit {
+    public cartItems: Product[] = [];
+    public totalPrice: number = 0;
+
+    constructor(private cartService: CartService) {}
+
+    public ngOnInit(): void {
+        this.cartService.getCartItems().subscribe((data) => {
+            this.cartItems = data;
+        });
+    }
+}
