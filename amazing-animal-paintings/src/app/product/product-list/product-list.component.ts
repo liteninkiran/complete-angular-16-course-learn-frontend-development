d814a43055a1a2d2c22d8db2b6c58fc5de8ebc58
Kiran Anand <liteninkiran@hotmail.com>
2025-08-18 15:27:13 +0100

    099 - Adding products to cart and popup

--- amazing-animal-paintings/src/app/product/product-list/product-list.component.ts
+++ amazing-animal-paintings/src/app/product/product-list/product-list.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+const snackBarConfig: MatSnackBarConfig<any> = {
+    duration: 2000,
+    horizontalPosition: 'center',
+    verticalPosition: 'top',
+};
+const snackBarMessage = 'Product added to cart';
 
 @Component({
     selector: 'app-product-list',
@@ -10,11 +19,23 @@ import { Product } from 'src/app/models/product';
 export class ProductListComponent implements OnInit {
     public products: Product[] = [];
 
-    constructor(private productService: ProductService) {}
+    constructor(
+        private productService: ProductService,
+        private cartService: CartService,
+        private snackbar: MatSnackBar,
+    ) {}
 
     public ngOnInit(): void {
         this.productService.getProducts().subscribe((data) => {
             this.products = data;
+        });
+    }
+
+    public addToCart(product: Product): void {
+        this.cartService.addToCart(product).subscribe({
+            next: () => {
+                this.snackbar.open(snackBarMessage, '', snackBarConfig);
+            },
         });
     }
 }
