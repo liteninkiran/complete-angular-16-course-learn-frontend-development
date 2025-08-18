92af63efd787ee3a7f66dc058fd34dc18f4f0562
Kiran Anand <liteninkiran@hotmail.com>
2025-08-18 12:58:48 +0100

    084 - Installing angular material and exploring cards

--- amazing-animal-paintings/src/app/app.module.ts
+++ amazing-animal-paintings/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ProductModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
