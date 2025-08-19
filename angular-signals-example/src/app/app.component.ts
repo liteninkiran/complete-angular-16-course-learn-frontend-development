eb8ee17ff8c38e0883a73415b87a32c5887c2d88
Kiran Anand <liteninkiran@hotmail.com>
2025-08-19 21:28:20 +0100

    143 - Signal Use Case

--- angular-signals-example/src/app/app.component.ts
+++ angular-signals-example/src/app/app.component.ts
@@ -1,19 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
+
+type Product = {
+    id: number;
+    name: string;
+    price: number;
+};
+
+const productArray: Product[] = [
+    { id: 1, name: 'Milk', price: 1.45 },
+    { id: 2, name: 'Bread', price: 3.9 },
+    { id: 3, name: 'Potatoes', price: 5.5 },
+];
+
+const filterProduct = (product: Product, filter: string) =>
+    product.name.toLowerCase().includes(filter.toLowerCase());
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
 export class AppComponent {
-    public title = 'angular-signals-example';
-    public price = 20;
-    public quantity = signal(10);
-    public totalPrice = computed(() => this.price * this.quantity());
+    public products = signal(productArray);
+    public filterName = signal('');
 
-    public changeQuantity(event: Event) {
-        this.quantity.set((event.target as HTMLInputElement).valueAsNumber);
+    private filterFn = (product: Product) =>
+        filterProduct(product, this.filterName());
+
+    public filteredProducts = computed(() =>
+        this.products().filter(this.filterFn),
+    );
+
+    public changeFilter(event: Event) {
+        const newFilterName = (event.target as HTMLInputElement).value;
+        this.filterName.set(newFilterName);
     }
 }
