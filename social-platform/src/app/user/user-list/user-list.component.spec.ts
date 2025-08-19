e4d2b3648c7a91898981e96fd946ef583b1dbe8a
Kiran Anand <liteninkiran@hotmail.com>
2025-08-19 19:24:31 +0100

    134 - Handling dependencies for component tests

--- social-platform/src/app/user/user-list/user-list.component.spec.ts
+++ social-platform/src/app/user/user-list/user-list.component.spec.ts
@@ -1,18 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { UserService } from '../user.service';
+import { of } from 'rxjs';
+import { User } from '../user.model';
+
+const users: User[] = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Maria Doe' },
+];
 
 describe('UserListComponent', () => {
     let component: UserListComponent;
     let fixture: ComponentFixture<UserListComponent>;
+    let userService: UserService;
+    let userServiceSpy: jasmine.Spy;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
             declarations: [UserListComponent],
-        });
+            providers: [UserService],
+        }).compileComponents();
+
         fixture = TestBed.createComponent(UserListComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
+
+        userService = TestBed.inject(UserService);
+        userServiceSpy = spyOn(userService, 'getUsers').and.returnValue(
+            of(users),
+        );
     });
 
     it('should create', () => {
