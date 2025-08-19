fa52e1c9aa90ff45bb3efedbceb791739fa62959
Kiran Anand <liteninkiran@hotmail.com>
2025-08-19 17:08:42 +0100

    114 - Creating a component and methods to dispatch our actions

--- book-management/src/app/app.module.ts
+++ book-management/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookReducer } from './books/book.reducer';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, BookListComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
