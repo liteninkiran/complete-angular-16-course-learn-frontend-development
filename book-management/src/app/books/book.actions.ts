e8f707d6cd77fa9041295e96f1f7b9083d9b15a6
Kiran Anand <liteninkiran@hotmail.com>
2025-08-19 17:36:49 +0100

    119 - Creating success and failure actions

--- book-management/src/app/books/book.actions.ts
+++ book-management/src/app/books/book.actions.ts
@@ -3,10 +3,15 @@ import { Book } from '../models/book';
 
 const messages = {
     add: '[Book] Add Book',
+    success: '[Book] Add Book Successfully',
+    failure: '[Book] Add Book Failure',
     remove: '[Book] Remove Book',
 };
 
+type Error = { error: any };
 type Remove = { bookId: string };
 
 export const AddBook = createAction(messages.add, props<Book>());
+export const AddBookSuccess = createAction(messages.success, props<Book>());
+export const AddBookFailure = createAction(messages.failure, props<Error>());
 export const RemoveBook = createAction(messages.remove, props<Remove>());
