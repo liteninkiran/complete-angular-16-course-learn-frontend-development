dba42b5a1cbcc7e3780aea6f3a65f78d7d46ab6d
Kiran Anand <liteninkiran@hotmail.com>
2025-08-19 17:45:31 +0100

    123 - Creating the effect part 2

--- book-management/src/app/books/book.effects.ts
+++ book-management/src/app/books/book.effects.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { BookService } from './book.service';
+import { mergeMap, map, catchError, of } from 'rxjs';
+import * as bookActions from './book.actions';
 
 @Injectable()
 export class BookEffects {
-    addBook$ = createEffect(() => {});
+    // This is an NgRx Effect that responds to 'AddBook' actions.
+    public addBook$ = createEffect(() =>
+        this.actions$.pipe(
+            // Listen for actions of type 'AddBook'
+            ofType(bookActions.AddBook),
+
+            // For each 'AddBook' action, call 'addBook' on the book service.
+            // 'mergeMap' allows multiple concurrent 'addBook' calls.
+            mergeMap((action) =>
+                this.bookService.addBook(action).pipe(
+                    // If the 'addBook' call is successful, dispatch 'AddBookSuccess' action with the book data.
+                    map((book) => bookActions.AddBookSuccess(book)),
+
+                    // If the 'addBook' call fails, dispatch 'AddBookFailure' action with the error.
+                    catchError((error) =>
+                        of(bookActions.AddBookFailure({ error })),
+                    ),
+                ),
+            ),
+        ),
+    );
 
     constructor(private actions$: Actions, private bookService: BookService) {}
 }
