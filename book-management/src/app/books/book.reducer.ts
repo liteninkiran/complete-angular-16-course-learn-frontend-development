56c989295d5b90542400021eb6acb9c4223c7658
Kiran Anand <liteninkiran@hotmail.com>
2025-08-19 17:01:53 +0100

    112 - Creating the book reducer part 2: Change functions

--- book-management/src/app/books/book.reducer.ts
+++ book-management/src/app/books/book.reducer.ts
@@ -1,6 +1,16 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { AddBook, RemoveBook } from './book.actions';
 import { Book } from '../models/book';
 
 export const initialState: ReadonlyArray<Book> = [];
 
-export const BookReducer = createReducer(initialState);
+export const BookReducer = createReducer(
+    initialState,
+    on(AddBook, (state, { id, title, author }) => [
+        ...state,
+        { id, title, author },
+    ]),
+    on(RemoveBook, (state, { bookId }) =>
+        state.filter((book) => book.id !== bookId),
+    ),
+);
